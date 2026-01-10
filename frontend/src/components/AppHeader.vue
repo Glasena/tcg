85a308a90a9059a9cd64f26326e04cf891e3d16a
Glasena <alisonglasenapp@gmail.com>
2026-01-10 16:20:33 -0300

    Add: AppHeader component

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Menubar from 'primevue/menubar'
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    route: '/',
+  },
+  {
+    label: 'Cards',
+    icon: 'pi pi-id-card',
+    items: [
+      {
+        label: 'List Cards',
+        icon: 'pi pi-list',
+        route: '/cards',
+      },
+      {
+        label: 'Create Card',
+        icon: 'pi pi-plus',
+        route: '/cards/create',
+      },
+    ],
+  },
+])
+</script>
+
+<template>
+  <div class="card">
+    <Menubar :model="items">
+      <template #start>
+        <img src="@/assets/logo.png" alt="Logo" class="h-10 w-auto" /> </template
+    ></Menubar>
+  </div>
+</template>
