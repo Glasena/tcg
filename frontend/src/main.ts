1856a3dbb743b4858c1e6c566d0fd2eed2312a6d
Glasena <alisonglasenapp@gmail.com>
2026-01-10 15:44:13 -0300

    Add: Prime vue lib.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import 'primeicons/primeicons.css' // ← SÓ ISSO
 
 import App from './App.vue'
 import router from './router'
@@ -8,5 +12,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 
 app.mount('#app')
