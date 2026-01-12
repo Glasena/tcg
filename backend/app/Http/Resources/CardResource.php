3a933dd51b1dfa80386074efccbbf1c1ad6b1cf3
Glasena <alisonglasenapp@gmail.com>
2026-01-12 20:32:32 -0300

    Add: include 'tcg_custom_id' in CardResource transformation

--- backend/app/Http/Resources/CardResource.php
+++ backend/app/Http/Resources/CardResource.php
@@ -20,6 +20,7 @@ class CardResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'tcg_type_id' => $this->tcg_type_id,
+            'tcg_custom_id' => $this->tcg_custom_id,
             'image_url' => $this->img_url
                 ? Storage::url($this->img_url)
                 : null,
